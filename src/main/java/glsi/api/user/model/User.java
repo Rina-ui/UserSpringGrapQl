4c4554042177e8445c5822e3dd4c2996755e5bae
Rina-ui <marinagracia895@gmail.com>
2025-12-30 10:36:14 +0000

    model user do

--- src/main/java/glsi/api/user/model/User.java
+++ src/main/java/glsi/api/user/model/User.java
@@ -1,4 +1,21 @@
 package glsi.api.user.model;
 
-public record User() {
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String sexe;
+    private Integer age;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
 }
+
